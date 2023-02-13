64782d0ade1105c137ae6147cbcb9e04f00b49a2
kueyeon0429 <20203065@kookmin.ac.kr>
2023-02-13 17:57:18 +0900

    :sparkles: add crud path

--- fast-api/crud_path.py
+++ fast-api/crud_path.py
@@ -20,14 +20,14 @@ def read_user(name: str):
     return {"nickname": USER_DB[name]}
 
 @app.put("/users/name/{name}/nickname/{nickname}")
-def edit_user(name: str, nickname: str):
+def update_user(name: str, nickname: str):
     if name not in USER_DB:
         raise NAME_NOT_FOUND
     USER_DB[name] = nickname
     return {"status": "success"}
 
 @app.delete("/users/name/{name}")
-def read_root(name: str):
+def delete_user(name: str):
     if name not in USER_DB:
         raise NAME_NOT_FOUND
     del USER_DB[name]
